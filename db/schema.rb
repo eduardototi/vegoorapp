75e93632119eb4955ab4f9ffae57065a75186368
Pedro Eduardo <eduardototi@gmail.com>
2020-12-14 20:26:54 -0300

    created clients views and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_182700) do
+ActiveRecord::Schema.define(version: 2020_12_14_203435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "razao_social"
+    t.string "cnpj"
+    t.string "ie"
+    t.string "email"
+    t.string "phone"
+    t.string "street"
+    t.string "number"
+    t.string "city"
+    t.string "state"
+    t.string "pais"
+    t.string "neighborhood"
+    t.string "cep"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_12_14_182700) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
 end
