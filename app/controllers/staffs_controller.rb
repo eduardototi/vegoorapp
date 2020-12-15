893e810881c201c9ecb9bd3f0bd4391952af4576
Pedro Eduardo <eduardototi@gmail.com>
2020-12-15 12:24:45 -0300

    staffs model views e controller

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,51 @@
+class StaffsController < ApplicationController
+  before_action :set_staff, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @staffs = Staff.all
+  end
+
+  def show
+  end
+
+  def new
+    @staff = Staff.new
+  end
+
+  def create
+    @staff = Staff.new(staff_params)
+    @staff.user = current_user
+    if @staff.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @staff.destroy
+    redirect_to staffs_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @staff.update(staff_params)
+      redirect_to staff_path(@staff)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_staff
+    @staff = Staff.find(params[:id])
+  end
+
+  def staff_params
+    params.require(:staff).permit(:first_name, :last_name, :email, :phone, :user_id)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :cleints
+  has_many :clients
+  has_many :staffs
 end
