fc17970696b424e75da2e1f183c6f52761936b86
Pedro Eduardo <eduardototi@gmail.com>
2020-12-21 16:14:10 -0300

    changes data base

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,12 +2,8 @@ class Order < ApplicationRecord
   belongs_to :staff
   belongs_to :user
   belongs_to :client
-  has_and_belongs_to_many :services
+  has_and_belongs_to_many :services, dependent: :destroy
   has_many :orderservices, dependent: :destroy
-  #has_many :services, through: :orderservices, dependent: :destroy
-  #accepts_nested_attributes_for :orderservices, allow_destroy: true
   has_many :orderequipments, dependent: :destroy
-  #has_many :equipments, through: :orderequipments, dependent: :destroy
-  #accepts_nested_attributes_for :orderequipments, allow_destroy: true
-  has_and_belongs_to_many :equipments
+  has_and_belongs_to_many :equipments, dependent: :destroy
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,4 @@
 class Service < ApplicationRecord
   has_many :orderservices, dependent: :destroy
-  #has_many :orders, through: :orderservices, dependent: :destroy
   has_and_belongs_to_many :orders
 end
