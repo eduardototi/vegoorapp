54d24d05b602125800ae92cd721e5a37ab3a8181
Pedro Eduardo <eduardototi@gmail.com>
2020-12-21 10:50:50 -0300

    order equipment done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clients
+
   resources :staffs
+
   resources :equipments
+
   resources :services
+
   resources :orders
+
+  resources :serviceorder do
+    resources :order
+  end
 end
