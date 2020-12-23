08704bd77f6715b3c63a6717a15d1b70c0df9ec8
Pedro Eduardo <eduardototi@gmail.com>
2020-12-23 17:45:53 -0300

    clients valdates

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,22 @@
 class Client < ApplicationRecord
+  STATES = ["AC", "AL", "AP", "AM", "BA", "CE", "DF",
+            "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+            "PB", "PR", "PE", "PI", "RJ", "RN", "RS",
+            "RO", "RR", "SC", "SP", "SE", "TO"]
+
   belongs_to :user
   has_many :service_orders
+  validates :cep, presence: true, format: { with: /\A[0-9]{5}-[0-9]{3}\z/, message: "CEP em formato invalido" }
+  validates :razao_social, presence: true, uniqueness: { case_sensitive: false, message: "Razão Social já cadastrada" }
+  validates :email, presence: true, uniqueness: {
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+    message: "Email invalido"
+  }
+  validates :phone, presence: true, uniqueness: true
+  validates :street, presence: true
+  validates :number, presence: true
+  validates :city, presence: true
+  validates :pais, presence: true
+  validates :neighborhood, presence: true
+  validates :cnpj, presence: true, uniqueness: true
 end
