d5feddcadf6039ca0f0779e2439dc3ffde663391
Pedro Eduardo <eduardototi@gmail.com>
2021-01-06 14:51:35 -0300

    users views show, index, edit created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   before_action :authorize_admin, only: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
 
   def new
     @user = User.new
@@ -10,19 +18,38 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path
     else
-      raise
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def authorize_admin
     current_user.admin?
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :client_id, :role, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :admin, :email, :client_id, :role, :password, :password_confirmation)
   end
 
 end
