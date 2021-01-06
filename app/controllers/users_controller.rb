03b067987dd2b4f83cda592a679fade4df2b962e
Pedro Eduardo <eduardototi@gmail.com>
2021-01-06 12:08:06 -0300

    new user ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :authorize_admin, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      raise
+      render :new
+    end
+  end
+
+  private
+
+  def authorize_admin
+    current_user.admin?
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :client_id, :role, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, except: :create
+  post 'create_user' => 'users#create', as: :create_user
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
