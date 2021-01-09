5635362ee75c87de813bae3ffbe86f40d1f2b029
Pedro Eduardo <eduardototi@gmail.com>
2021-01-09 19:26:05 -0300

    add service type to orders

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,11 +25,12 @@ require("@nathanvda/cocoon")
 
 // External imports
 import "bootstrap";
+import 'select2/dist/css/select2.css';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  //initSelect2();
 });
