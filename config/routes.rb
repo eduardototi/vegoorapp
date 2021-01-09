eedcab55c99232a75a95d071d652299e8116e81a
Pedro Eduardo <eduardototi@gmail.com>
2021-01-09 11:21:48 -0300

    Staff table deleted

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clients
 
-  resources :staffs
-
   resources :utensils
 
   resources :services
