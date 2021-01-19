15504a7bcc5ebb712cefe7678e929268e456cd79
Pedro Eduardo <eduardototi@gmail.com>
2021-01-19 15:21:41 -0300

    vegoor reports views edit and index created

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :client
   belongs_to :contact, class_name: "User"
-  belongs_to :vegoor_report
   has_many :orderservices, dependent: :destroy
   has_many :services, through: :orderservices
   accepts_nested_attributes_for :orderservices, reject_if: :all_blank, allow_destroy: true
