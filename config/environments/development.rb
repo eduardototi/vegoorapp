be3ea89ae160e9dbccf0a8399e822c5078c9130d
Pedro Eduardo <eduardototi@gmail.com>
2021-02-05 10:34:00 -0300

    email key changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.smtp_settings = {
   :user_name => 'eduardototi@gmail.com',
-  :password => 'Ed0684ed@Ed0684ed@',
+  :password => EMAIL_KEY,
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
