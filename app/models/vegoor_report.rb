9ce841f8589586f46576c9840c74ab819ab7b288
Pedro Eduardo <eduardototi@gmail.com>
2021-02-16 13:34:13 -0300

    report e-mails config

--- app/models/vegoor_report.rb
+++ app/models/vegoor_report.rb
@@ -2,8 +2,8 @@ class VegoorReport < ApplicationRecord
     belongs_to :order
     has_many :orderservices, through: :orders
     has_many_attached :photos
-    # after_create :send_confirmation
-    # after_update :send_confirmation
+    after_create :send_confirmation
+    after_update :send_confirmation
     
     private
 
